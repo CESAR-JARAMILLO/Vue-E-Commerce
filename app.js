8fe3e6f590047de80469e91bee455cc07f277164
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-15 07:35:26 -0800

    create Vue instance and connect to DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+// Creates new Vue instance
+// Root of application, connects to element in the DOM
+// That element can use a {{ expression }} to display that instances data
+// Contains multiple option properties used to store data and perform actions
+var app = new Vue({
+    el: '#app', // element property, connects to div with "app" id
+    data: {
+        product: 'Socks'
+    }
+})
