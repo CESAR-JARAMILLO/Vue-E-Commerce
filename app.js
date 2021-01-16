845ae28d5562014e9298a7475da9d928cfc916f0
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-16 12:45:15 -0800

    components

--- app.js
+++ app.js
@@ -1,34 +1,103 @@
-// Creates new Vue instance
-// Root of application, connects to element in the DOM
-// That element can use a {{ expression }} to display that instances data
-// Contains multiple option properties used to store data and perform actions
-var app = new Vue({
-    el: '#app', // element property, connects to div with "app" id
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        description: 'A pair of warm, fuzzy socks',
-        selectedVariant: 0,
-        link: 'https://www.vuemastery.com',
-        onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "Green",
-                variantImage: './images/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "Blue",
-                variantImage: './images/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XLL'],
-        cart: 0
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
     },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+            
+    <!-- v-bind dynamicly binds an attribute to an expression -->
+    <!-- The attribute her is "src", and the expression is inside the quotes "product-image" -->
+   <!-- The short hand is just the colon whithout "v-bind" -->
+    <div class="product-image">
+        <!-- Creates a bond between the data and the attribute -->
+        <img v-bind:src="image">
+        <a :href="link">View Image</a>
+    </div>                
+    
+    <div class="product-info">
+        <!-- Pulls from Vue instance data using expression -->
+        <h1>{{ title }}</h1> 
+        <p>{{ description }}</p>
+        <p>Shipping: {{ shipping }}</p>
+        <!-- unlike v-if, v-show toggles visiblity off and on -->
+        <!-- v-if removes or adds element, not as efficient -->
+        <p v-show="inStock">In Stock</p>
+        <p :class="{ outOfStock: !inStock }"
+        v-show="!inStock">Out of Stock</p>
+        <span v-if="onSale">{{ sale }}</span>
+
+        <!-- v-for can be used to iterate over an array to display data -->
+        <product-details :details="details"></product-details>
+
+        <!-- When using v-for it's recommended to give each rendered element it's own unique key. -->
+        <div v-for="(variant, index) in variants" 
+        :key="variant.variantId"
+        class="color-box"
+        :style="{ backgroundColor: variant.variantColor }"
+        @mouseover="updateProduct(index)">
+        </div>
+
+        <ul>
+            <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+
+        <button v-on:click="addToCart" 
+        :disabled="!inStock"
+        :class="{ disabledButton: !inStock }">Add to cart</button>
+        <button v-on:click="removeFromCart">Remove from cart</button>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+
+    </div>
+
+</div>
+    `,
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            description: 'A pair of warm, fuzzy socks',
+            selectedVariant: 0,
+            link: 'https://www.vuemastery.com',
+            onSale: true,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "Green",
+                    variantImage: './images/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "Blue",
+                    variantImage: './images/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XLL'],
+            cart: 0
+        }
+    }, 
     methods : {
         addToCart() {
             this.cart += 1
@@ -54,6 +123,23 @@ var app = new Vue({
             if (this.onSale) {
                 return this.brand + ' ' + this.product + ' are on sale!'
             }
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
         }
+    }
+})
+
+// Creates new Vue instance
+// Root of application, connects to element in the DOM
+// That element can use a {{ expression }} to display that instances data
+// Contains multiple option properties used to store data and perform actions
+var app = new Vue({
+    el: '#app', // element property, connects to div with "app" id
+    data: {
+        premium: true
     }
 })
